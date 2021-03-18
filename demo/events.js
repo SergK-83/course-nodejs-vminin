5358f7abe041c082b7c9d6cff48ef36d64799dcb
Сергей Крюков <krus-83@mail.ru>
2021-03-18 16:27:15 +0700

    36:40 EventEmitter

--- demo/events.js
+++ demo/events.js
@@ -1,3 +1,18 @@
 const EventEmitter = require('events');
 
-const emitter = new EventEmitter();
+// const emitter = new EventEmitter();
+//
+// emitter.on('anything', data => {
+//   console.log('ON: anything', data);
+// });
+//
+// emitter.emit('anything', {a: 1}); // получаем прослушку события 'anything'
+// emitter.emit('anything', {b: 1});
+//
+// setTimeout(() => {
+//   emitter.emit('anything', {c: 3});
+// }, 1000);
+
+class Dispatcher extends EventEmitter {
+
+}
