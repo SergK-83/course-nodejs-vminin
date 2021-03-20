db674ad17725257dce1229273341f2c155f4188f
Sergey Kryukov <krus-83@mail.ru>
2021-03-20 13:07:42 +0700

    server updated

--- index.js
+++ index.js
@@ -45,19 +45,49 @@ const server = http.createServer((req, res) => {
   // }
 
   let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+  let contentType = 'text/html';
 
-  fs.readFile(filePath, (err, data) => {
+  switch (ext) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    default:
+      contentType = 'text/html';
+  }
+
+  if (!ext) {
+    filePath +='.html';
+  }
+
+  fs.readFile(filePath, (err, content) => {
     if (err) {
-      fs.readFile(path.join(__dirname, 'public'))
-    }
+      fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+        if (err){
+          res.writeHead(500);
+          res.end('Error');
+        } else {
+          res.writeHead(200, {
+            'Cotent-Type': 'text/html'
+          });
+          res.end(data);
+        }
+      });
 
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    res.end(data);
+    } else {
+      res.writeHead(200, {
+        'Content-Type': contentType
+      });
+      res.end(content);
+    }
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server started...');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
